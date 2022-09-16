82a3b28120ba300922ce80de718588ff54ad8c92
Joe Maloney <maloney.jg2@gmail.com>
2022-09-16 09:45:38 -0400

    tsx in main

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import './App.css';
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> yo this shit working
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
