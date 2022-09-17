0c75fcb6b66fcec651d91da102afc4384677fe54
joemowed <maloney.jg2@gmail.com>
2022-09-17 11:02:10 -0400

    added build

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="navbar">
       
-        <p className="mx-auto w-20 bg-yellow-500 my-8 rounded-md" >yo this shit working</p>
+        <p className="mx-auto h-20  bg-blue-500/60  rounded-b-xl" >yo this shit working</p>
         
     </div>
   );
